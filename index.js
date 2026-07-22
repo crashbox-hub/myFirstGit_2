ace3037b6c0a62419c887d7ec4a4632345fb3d49
Christopher Burrell <christopher.n.burrell@gmail.com>
2026-07-23 09:48:05 +1000

    added adding function

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 console.log("Hello, World!");
+
+// Function to add two numbers 5+10
+function addNumbers(a, b) {
+    return a + b;
+}
+
+const result = addNumbers(5, 10);
+console.log("The sum of 5 and 10 is:", result);
